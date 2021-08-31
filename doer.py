5b9d356c1e48e80a504719e5277150dd43f36f90
SrKoDes <desravines.kohiin@gmail.com>
2021-08-31 19:21:57 -0400

    Inital commit with some completed work

---
+++ doer.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+def add_Koala_Class():
+    pass
+
+def welcome():
+    welcome_message = "Welcome to the Koala world!"
+    return welcome_message
+
+def choice():
+    choice_message = "Choose your class: Bear, Queenslander, Phascolarctos, Joey"
+    return(choice_message)
