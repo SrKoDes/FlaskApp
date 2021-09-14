57c4615e92ef947167f7fee51a4926896a8558a7
SrKoDes <desravines.kohiin@gmail.com>
2021-09-14 19:07:53 -0400

    new working attack button

--- static/js/script.js
+++ static/js/script.js
@@ -6,8 +6,9 @@
 const actionMenu = document.getElementById('actions');
 const attackButton = document.getElementById('attack');
 const abilitiesButton = document.getElementById('abilities');
-var character_data = []
-var enemy_data = []
+let character_data = []
+let enemy_data = []
+let hp = 0
 //abilitiesMenu elements
 const abilitiesMenu = document.getElementById('abilitiesMenu');
 
@@ -19,20 +20,44 @@ attackButton.addEventListener('click', attack);
 abilitiesButton.addEventListener('click', goAbilitiesMenu);
 
 function attack() {
-  fetch(`/character/info?element=${playerName.innerHTML}`)
-  .then( result => result.json())
-  .then( data => {
-    character_data = data
-  });
-
-  fetch(`/character/info?element=${enemyName.innerHTML}`)
-  .then( result => result.json())
-  .then( data => {
-    enemy_data = data
-  });
-
-  
+  // get_character_info()
+  // get_enemy_info()
+  // console.log(enemy_data)
+  // console.log("HP: " + enemy_data[1] - character_data[2] + enemy_data[3])
+  //  - playerHP.innerHTML.slice(4) + enemyArmor.innerHTML.slice(7)
+  enemyHP.innerHTML = `HP: ${enemyHP.innerHTML.slice(4) - playerDMG.innerHTML.slice(8) + enemyArmor.innerHTML.slice(8)}`
+  enemyAttack()
 }
+
+function enemyAttack() {
+  playerHP.innerHTML = `HP: ${playerHP.innerHTML.slice(4) - enemyDMG.innerHTML.slice(8) + playerArmor.innerHTML.slice(8)}`
+}
+
+
+// function get_character_info() {
+//   fetch(`/character/info?element=${playerName.innerHTML}`)
+//   .then( result => result.json())
+//   .then( data => {
+//     return character_data.push(data)
+//   });
+// }
+
+
+
+// function get_enemy_info() {
+//   fetch(`/character/info?element=${enemyName.innerHTML}`)
+//   .then( result => result.json())
+//   .then( data => {
+//     return enemy_data.push(data)
+//   });
+// }
+
+
+function goAbilitiesMenu() {
+  actionMenu.parentNode.removeChild(actionMenu)
+  abilitiesMenu.style.display = "flex"
+}
+
 
 function populatePlayerDiv() {
   fetch(`/character/info?element=${roleName}`)
@@ -48,11 +73,6 @@ function populatePlayerDiv() {
   playerPic.innerHTML = `<img src = ${playerPicUrl}/>`
 }
 
-
-function goAbilitiesMenu() {
-  actionMenu.parentNode.removeChild(actionMenu)
-  abilitiesMenu.style.display = "flex"
-}
 
 
 function getRandomDog() {
